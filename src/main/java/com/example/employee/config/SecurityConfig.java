2f07f43093fbe0a973dfc8b3d71cd06e5b7f044c
prabhug1987 <prabhug1987@gmail.com>
2021-09-04 20:22:13 +0530

    spring security code commented out

--- src/main/java/com/example/employee/config/SecurityConfig.java
+++ src/main/java/com/example/employee/config/SecurityConfig.java
@@ -8,8 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	super.configure(http);
-        http.authorizeRequests().anyRequest().permitAll()  
-            .and().csrf().disable();
+    	//super.configure(http);
+        //http.authorizeRequests().anyRequest().permitAll()  
+          //  .and().csrf().disable();
     }
 }
